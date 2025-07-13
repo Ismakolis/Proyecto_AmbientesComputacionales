1dfe7bbe062ba71e50bc7c761af3a07cdfa7abed
Ismakolis <esteban.isamel.canencia@gmail.com>
2025-07-12 22:19:46 -0500

    Primeros avances (Estructura del proyecto)

---
+++ Servidor/index.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const conectarDB = require('./config/db');
+const cors = require("cors");
+require('dotenv').config();
+const cookieParser = require('cookie-parser');
+const verificarToken = require('./middleware/auth');
+
+// Creamos el servidor 
+const app = express();
+
+// Conectamos a la base de datos
+conectarDB();
+
+// Configurar CORS correctamente para aceptar cookies
+const corsOptions = {
+  origin: 'http://localhost:4200',
+  credentials: true
+};
+app.use(cors(corsOptions));
+
+// Middlewares
+app.use(express.json());
+app.use(cookieParser());
+
+// Rutas
+app.use('/api/loginUser', require('./routes/usuarioLogin'));
+
+
+
+//usuarios
+app.use('/api', require('./routes/usuarioRoutes'));
+
+app.get('/api/usuarioSesion', verificarToken, require('./routes/usuarioRoutes'));
+
+
+
+
+
+// Iniciar servidor
+app.listen(4000, () => {
+  console.log('El servidor arrancó perfectamente en el puerto 4000');
+});
