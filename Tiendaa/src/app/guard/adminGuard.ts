5f0ecc5b78270e1a3bf026d8ca1f2a07d92d1e44
Ismakolis <esteban.isamel.canencia@gmail.com>
2025-07-13 18:34:14 -0500

    Arregle el mensaje de alerta de angular causado por el LocalStorage

--- Tiendaa/src/app/guard/adminGuard.ts
+++ Tiendaa/src/app/guard/adminGuard.ts
@@ -1,14 +1,23 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Inject, PLATFORM_ID } from '@angular/core';
 import { CanActivate, Router } from '@angular/router';
+import { isPlatformBrowser } from '@angular/common';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdminGuard implements CanActivate {
 
-  constructor(private router: Router) {}
+  constructor(
+    private router: Router,
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) {}
 
   canActivate(): boolean {
+    if (!isPlatformBrowser(this.platformId)) {
+      this.router.navigate(['/acceso-denegado']);
+      return false;
+    }
+
     const usuario = JSON.parse(localStorage.getItem('usuario') || '{}');
     const rol = usuario?.rol || 'cliente';
 
