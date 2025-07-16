e98ce7a5baf20e57db25ea387730e6e52cf326ff
AlejandroCaval <Alejandro22caval@gmail.com>
2025-07-15 22:40:15 -0500

    Agrega componente proveedor con CRUD

---
+++ Tiendaa/src/app/Bunisess/prov/prov.ts
@@ -0,0 +1,73 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-prov',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './prov.html',
+  styleUrls: ['./prov.css']
+})
+export class Prov implements OnInit {
+  proveedores: any[] = [];
+  proveedorForm: FormGroup;
+  editando: boolean = false;
+  proveedorEditandoId: string = '';
+
+  constructor(private http: HttpClient, private fb: FormBuilder) {
+    this.proveedorForm = this.fb.group({
+      nombre: [''],
+      telefono: [''],
+      email: [''],
+      direccion: ['']
+    });
+  }
+
+  ngOnInit(): void {
+    this.obtenerProveedores();
+  }
+
+  obtenerProveedores(): void {
+    this.http.get<any[]>('http://localhost:3000/api/proveedores')
+      .subscribe(data => {
+        this.proveedores = data;
+      });
+  }
+
+  guardarProveedor(): void {
+    const data = this.proveedorForm.value;
+
+    if (this.editando) {
+      this.http.put(`http://localhost:3000/api/proveedores/${this.proveedorEditandoId}`, data)
+        .subscribe(() => {
+          this.obtenerProveedores();
+          this.cancelarEdicion();
+        });
+    } else {
+      this.http.post('http://localhost:3000/api/proveedores', data)
+        .subscribe(() => {
+          this.obtenerProveedores();
+          this.proveedorForm.reset();
+        });
+    }
+  }
+
+  editarProveedor(prov: any): void {
+    this.editando = true;
+    this.proveedorEditandoId = prov._id;
+    this.proveedorForm.patchValue(prov);
+  }
+
+  eliminarProveedor(id: string): void {
+    this.http.delete(`http://localhost:3000/api/proveedores/${id}`)
+      .subscribe(() => this.obtenerProveedores());
+  }
+
+  cancelarEdicion(): void {
+    this.editando = false;
+    this.proveedorEditandoId = '';
+    this.proveedorForm.reset();
+  }
+}
